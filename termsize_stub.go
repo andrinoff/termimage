43ea445a5ed4e23567547fceaddfcddc9a8257b6
drew <me@andrinoff.com>
2026-05-27 18:00:09 +0400

    format
    Signed-off-by: drew <me@andrinoff.com>

--- termsize_stub.go
+++ termsize_stub.go
@@ -4,6 +4,6 @@ package termimage
 
 import "os"
 
-func termPixels(_ *os.File) (int, int)             { return 1920, 1080 }
-func termChars(_ *os.File) (int, int)              { return 220, 50 }
-func cellPixels(_ *os.File) (cellW, cellH int)     { return 8, 16 }
+func termPixels(_ *os.File) (int, int)         { return 1920, 1080 }
+func termChars(_ *os.File) (int, int)          { return 220, 50 }
+func cellPixels(_ *os.File) (cellW, cellH int) { return 8, 16 }
